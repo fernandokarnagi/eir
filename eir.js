f75ed2ab2e27df159acf9e4a3aa71fc14f2ab626
Fernando Karnagi <fernando@cyder.com.sg>
2022-05-02 09:20:04 +0800

    update

--- eir.js
+++ eir.js
@@ -1,3 +1,5 @@
+import moment from 'moment'
+
 const PMT = (rate, nper, pv, fv, type) => {
     if (!fv) fv = 0;
     if (!type) type = 0;
@@ -14,17 +16,42 @@ const PMT = (rate, nper, pv, fv, type) => {
     return pmt.toFixed(2) * -1;
 }
 
-const loanAmount = 1000; // principal
+const loanAmount = 1000;
 const tenor = 12; // in months
 const eir = 35.074;
 const nominalInterestRateMonthly = 1;
-const startMonth = "02/2022";
+const startMonth = "04/2022";
+const nominalInterestRateAnnually = nominalInterestRateMonthly * tenor / 100;
+const totalInterest = nominalInterestRateAnnually * loanAmount;
+
+const startMoment = moment(startMonth, "MM/YYYY").add(-1, "month");
+let outstandingPrincipal = loanAmount;
 
 const pmt = PMT(eir / (12 * 100), tenor, loanAmount);
-// console.log(pmt);
 
+console.log("Interest Rate Calculator");
+console.log("----------------------------");
+console.log("Loan amount S$         : " + loanAmount);
+console.log("EIR (%)                : " + eir);
+console.log("Tenor (months)         : " + tenor);
+console.log("Total interest S$      : " + totalInterest);
+console.log("Start month            : " + moment(startMonth, "MM/YYYY").format("MMM-YYYY"));
+console.log("Monthly installment S$ : " + pmt);
+console.log("----------------------------");
+
+let accumInterest = 0;
 for (let i = 0; i < tenor; i++) {
-    
-    const principal = pmt;
+    const newMoment = startMoment.add(1, "month");
+    const numOfDaysInMonth = newMoment.daysInMonth();
+
+    const interest = (eir / 100) / 365 * numOfDaysInMonth * outstandingPrincipal;
+    const principalPaid = (i == tenor - 1) ? outstandingPrincipal : pmt - interest;
+    outstandingPrincipal -= principalPaid;
+    console.log(newMoment.format("MMM-YYYY"), pmt, principalPaid, interest, outstandingPrincipal);
+    accumInterest += interest;
 
 }
+console.log("----------------------------");
+const differentOfInterestVal = accumInterest - totalInterest;
+console.log("Total accumulated interest S$ : " + accumInterest);
+console.log("Difference in interest S$ : " + differentOfInterestVal);
